0eec02a80ee9dece65fff3b5a0e79c43ec137c2b
emisemisRoxo <156017135+emisemisRoxo@users.noreply.github.com>
2024-02-01 16:07:20 -0300

    Add files via upload

---
+++ controller/InterfacePadoka.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class InterfacePadoka {
+   // aqui você pode declarar todas as variáveis de ambiente / interface do sistema padoka
+   public static int varInt = 2;
+   public static int qtdFarinha = 0;
+}
